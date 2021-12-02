99542827d4c6b01a963d4cc7d6acb597b3442911
thefubon <43171309+thefubon@users.noreply.github.com>
2021-12-02 19:15:28 +0300

    update Add Icon

--- editor.php
+++ editor.php
@@ -92,10 +92,10 @@ $title	= "Fubon CMS | Editor ⚙️";
 			</svg>
 		</button>
 		<a href="/" target="_blank"><img class="w-[18px] md:w-[28px]" src="/assets/img/fubon-one.svg" alt="Fubon CMS"></a>
-		<a class="hidden md:mt-6 md:flex md:flex-col items-center space-x-1 md:space-x-0 md:space-y-1" href="#!" onclick="return newPage()" title="Add New Page">
-			<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-6 h-6" viewBox="0 0 16 16">
-				<path d="m.5 3 .04.87a1.99 1.99 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2zm5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19c-.24 0-.47.042-.683.12L1.5 2.98a1 1 0 0 1 1-.98h3.672z"/>
-				<path d="M13.5 10a.5.5 0 0 1 .5.5V12h1.5a.5.5 0 1 1 0 1H14v1.5a.5.5 0 1 1-1 0V13h-1.5a.5.5 0 0 1 0-1H13v-1.5a.5.5 0 0 1 .5-.5z"/>
+		<a class="hidden md:mt-6 md:flex md:flex-col items-center space-x-1 md:space-x-0 md:space-y-1 group" href="#!" onclick="return newPage()" title="Add New Page">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-6 h-6 group-hover:text-[#6563FD]" viewBox="0 0 16 16">
+				<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+				<path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
 			</svg>
 			<span class="text-xs">Add</span>
 		</a>
